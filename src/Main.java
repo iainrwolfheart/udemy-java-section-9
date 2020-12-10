01127507c9e05f3a281406eec9a367be688881c9
iainrwolfheart <iainrgorrie@gmail.com>
2020-12-10 20:42:38 +0000

    S9 basically finished

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         MyLinkedList list = new MyLinkedList(null);
+        SearchTree tree = new SearchTree(null);
+
         list.traverse(list.getRoot());
 
         String stringData = "London Bristol Cardiff Brighton Swindon";
---
+++ src/SearchTree.java
@@ -0,0 +1,34 @@
+
+
+public class SearchTree implements NodeList {
+
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem item) {
+        if (root != null) {
+            traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+    }
+}
